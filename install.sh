b87e5af30e7227fd77142bbd0be66a8ef017c136
mafaz-veuz <mafaz@veuz.in>
2025-07-18 10:34:15 +0530

    2nd Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 cd Documents
 sudo dpkg -i Flow-Desq-1.0.7.deb
+echo "Installing with Org ID: $organizationId"
+
 echo "{\"organizationId\": \"$organizationId\"}" | sudo tee /usr/local/bin/config.json > /dev/null
-sudo chmod 777 /usr/local/bin/config.json
+sudo chmod 644 /usr/local/bin/config.json
+
+echo "✅ Installed with org ID: $organizationId"
