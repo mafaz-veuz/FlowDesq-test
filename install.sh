b6fc536c1f23be1c23ef8b849d8e9bf3a8380efb
mafaz-veuz <mafaz@veuz.in>
2025-07-18 10:29:41 +0530

    1st Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+cd Documents
 sudo dpkg -i Flow-Desq-1.0.7.deb
 echo "{\"organizationId\": \"$organizationId\"}" | sudo tee /usr/local/bin/config.json > /dev/null
 sudo chmod 777 /usr/local/bin/config.json
